a707a6e140edf3f1d21b587bf010f215ab296eee
Kuan314 <58419606+Kuan314@users.noreply.github.com>
2021-05-10 15:58:17 +0800

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -2,28 +2,28 @@ package HW2;
 
 public class Bicycle implements CarbonFootprint{
 
-    private double miles;
-    private static double footprint_per_mile_average = 50;
+    private double distances;
+    private static double footprint_per_km_average = 31.07;
 
-    public double getMiles(){
-        return miles;
+    public double getDistances(){
+        return distances;
     }
-    public double getFootprint_per_mile_average(){
-        return footprint_per_mile_average;
-    }
-
-    public double setMiles(miles){
-        this.miles = miles;
+    public double getFootprint_per_km_average(){
+        return footprint_per_km_average;
     }
 
-    public Bicycle(double miles){
-        setMiles(miles);
+    public double setDistances(distances){
+        this.distances = distances;
+    }
+
+    public Bicycle(double distances){
+        setDistances(distances);
     }
 
     @Override
     public double getCarbonFootprint() {
         // TODO Auto-generated method stub
-        double footprint = getMiles() * getFootprint_per_mile_average();
+        double footprint = getDistances() * getFootprint_per_km_average();
 
         return footprint;
     }
